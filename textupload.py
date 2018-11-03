25ab918e59e7a34764da9221e3a3ad1315aa01a6
Evan Hruskar <hruskar.evan@gmail.com>
2018-11-03 11:02:59 -0400

    Doesn't authenticate, but it doesn't crash

--- textupload.py
+++ textupload.py
@@ -4,35 +4,38 @@ import base64
 import hashlib
 import datetime
 
+KEY = "ALTR-24E95DD903235BE18B130C1CAA0923ED"
+SECRET = "4df873715bde1fdf27d751619f0aca3d199fcc32dce112b988ce078287a76a39"
+URL = "https://dgl-hackathon.dev.altr.com/api/v1/chain"
+
 def generateHMAC(payload, method, resource = ""):
     print("HMAC Payload: ", payload)
     print("HMAC Method: ", method)
     fullpayload = method + "\n" + resource + "\n" + payload + "\n"
     print(fullpayload)
     # secureHash = hashlib.sha256(fullpayload, SECRET)
-    secureHash = hmac.new(SECRET, fullpayload, hashlib.sha256)
-    base64.b64encode(secureHash).decode()
-    return secureHash
+    # secureHash = hmac.new(bytearray(SECRET, 'utf-8'), bytearray(fullpayload, 'utf-8'), hashlib.sha256).digest()
+    # base64.b64encode(secureHash).decode()
+    digest = hmac.new(bytearray(SECRET, 'utf-8'), msg=bytearray(payload, 'utf-8'), digestmod=hashlib.sha256).digest()
+    signature = base64.b64encode(digest).decode()
+    return signature
 
 # url = "https://dgl-hackathon.dev.altr.com/api/v1/chain/chain_618f5f6485a9c93f970846193207027c08d0634962460e3e46069692daa70d3e"
-KEY = "ALTR-24E95DD903235BE18B130C1CAA0923ED"
-SECRET = "4df873715bde1fdf27d751619f0aca3d199fcc32dce112b988ce078287a76a39"
-URL = "https://dgl-hackathon.dev.altr.com/api/v1/chain"
 date = datetime.datetime.now().isoformat();
-AUTH = "ALTR " + KEY + ":" + generateHMAC(date, POST)
+AUTH = "ALTR " + KEY + ":" + generateHMAC(date, "POST")
 print(AUTH)
 #
-# headers = {
-#     'Authorization': "{{altrauth}}",
-#     'Content-Type': "application/json",
-#     'X-ALTR-DATE': "{{altrdate}}",
-#     'cache-control': "no-cache",
-#     'Postman-Token': "1c52a41a-9850-4999-a70f-7de183d9048c"
-#     }
+headers = {
+    'Authorization': AUTH,
+    'Content-Type': "application/json",
+    'X-ALTR-DATE': date,
+    'cache-control': "no-cache",
+    'Postman-Token': "1c52a41a-9850-4999-a70f-7de183d9048c"
+    }
 #
-# response = requests.request("GET", url, headers=headers)
+response = requests.request("GET", URL, headers=headers)
 #
-# print(response.text)
+print(response.text)
 
 
 # pm.environment.set("altrkey", KEY)
