d2ddb4e04562137c6a49b84aca064a8d74a89047
Anthony Dellinger <18dellingera@bubblers.us>
2018-11-03 12:56:38 -0400

    aaaaaah

--- working/webcam.py
+++ working/webcam.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import os
+
 
 cap = cv2.VideoCapture(0)
 
@@ -9,10 +11,10 @@ xvid = cv2.VideoWriter_fourcc(*'XVID')
 xvid_out = cv2.VideoWriter('xvid.avi', xvid, 5.0, (640, 480))
 
 i = 0
-while True:
-    i+=1
-    xvid_out = cv2.VideoWriter(str(i) + '.avi', xvid, 10.0, (640, 480))
-    for x in range(5 * 10):
+for x in range(5):
+    i += 1
+    xvid_out = cv2.VideoWriter('videos/' + str(i) + '.avi', xvid, 10.0, (640, 480))
+    for y in range(5 * 30):
         try:
             ret, frame = cap.read()
             xvid_out.write(frame)
@@ -22,3 +24,5 @@ while True:
             cap.release()
             xvid_out.release()
             cv2.destroyAllWindows()
+    command = 'ffmpeg -i videos/' + str(i) + '.avi videos/' + str(i) + '.mp4 && node upload.js ' + str(i)
+    os.system(command)
